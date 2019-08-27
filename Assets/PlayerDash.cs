4ec92d4466af58524b7411074f022f784571dad1
Luxiere <49752840+Luxiere@users.noreply.github.com>
2019-08-28 01:32:23 +0700

    Player Movement and Character Model
    Character Model - klaiis
    Player Movement - Lux

---
+++ Assets/PlayerDash.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerDash : MonoBehaviour
+{
+    [SerializeField] float dashSpeed = 10f;
+    [SerializeField] float dashTime = .25f;
+    [SerializeField] float dashCooldown = 2f;
+    [SerializeField] GameObject dashTarget;
+
+    Rigidbody2D rb;
+    PlayerMovement playerMovement;
+    Vector2 dashTargetPos;
+
+    float currentDashTime = 0f;
+    float dashCooldownTimer;
+
+    bool isDashing = false;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        playerMovement = GetComponent<PlayerMovement>();
+    }
+       
+    public void Dash()
+    {
+        currentDashTime += Time.deltaTime;
+        if (currentDashTime > dashTime)
+        {
+            currentDashTime = 0;
+            playerMovement.SetIsDashing(false);
+        }
+        float perc = currentDashTime / dashTime;
+        transform.position = Vector2.Lerp(transform.position, dashTargetPos, perc);
+    }
+
+    public void CacheDashTarget()
+    {
+        dashTargetPos = dashTarget.transform.position;
+    }
+
+    public void DashCooldown()
+    {
+        dashCooldownTimer -= Time.deltaTime;
+        if(dashCooldownTimer <= Mathf.Epsilon)
+        {
+            dashCooldownTimer = dashCooldown;
+            playerMovement.SetDashable(true);
+        }
+    }
+}
---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [Header("Player Movement")]
+    [SerializeField] float moveSpeed = 5f;
+
+    Rigidbody2D rb;
+    PlayerDash playerDash;
+
+    bool dashable = true;
+    bool isDashing = false;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        playerDash = GetComponent<PlayerDash>();
+    }
+
+    void Update()
+    {
+        Dash();
+        DashControl();
+        DashCooldown();
+        if (!isDashing)
+        {
+            Movement();
+        }
+    }
+
+    private void Movement()
+    {
+        float controlThrowHorizontal = Input.GetAxis("Horizontal");
+        float controlThrowVertical = Input.GetAxis("Vertical");
+        rb.velocity = new Vector2(controlThrowHorizontal * moveSpeed, controlThrowVertical * moveSpeed);
+    }
+
+    private void DashControl()
+    {
+        if (Input.GetButtonDown("Dash"))
+        {
+            if (dashable)
+            {
+                isDashing = true;
+                dashable = false;
+                playerDash.CacheDashTarget();
+            }
+        }
+    }
+
+    private void DashCooldown()
+    {
+        if (!dashable)
+        {
+            playerDash.DashCooldown();
+        }
+    }
+
+    private void Dash()
+    {
+        if (isDashing)
+        {
+            playerDash.Dash();
+        }
+    }
+
+    private void Direction()
+    {
+        if (Mathf.Abs(rb.velocity.x) > Mathf.Epsilon || Mathf.Abs(rb.velocity.y) > Mathf.Epsilon)
+        {
+            transform.localScale = new Vector2(Mathf.Sign(rb.velocity.x), Mathf.Sign(rb.velocity.y));
+        }        
+    }
+
+    public void SetIsDashing(bool isDashing)
+    {
+        this.isDashing = isDashing;
+    }
+
+    public void SetDashable(bool dashable)
+    {
+        this.dashable = dashable;
+    }
+}
